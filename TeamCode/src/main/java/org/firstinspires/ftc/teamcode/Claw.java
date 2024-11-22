d74d412306c18ad3ab5a3f083037878d5b36c192
Daniel <cortezdaniel707@gmail.com>
2024-11-22 14:08:36 -0800

    Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -5,48 +5,25 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
-    private static Servo leftServo;
-    private static Servo rightServo;
+    private static Servo Claw;
     private static Gamepad Driver1;
     private static Gamepad Driver2;
-    private static double scorePosition = 0.50;
-    private static double intakePosition = 0.065;
-    static boolean ArmIsUp;
-    static int Count = 0;
+    private static double Close = 0;
+    private static double Open = 0;
 
     public Claw(OpMode opMode) {
         Driver1 = opMode.gamepad1;
         Driver2 = opMode.gamepad2;
-        leftServo = (Servo) opMode.hardwareMap.get("L claw");
-        rightServo = (Servo) opMode.hardwareMap.get("R claw");
-
-
-        leftServo.setDirection(Servo.Direction.REVERSE);
-        rightServo.setDirection(Servo.Direction.FORWARD);
-        armServo(scorePosition, scorePosition);
-        ArmIsUp=true;
+        Claw = (Servo) opMode.hardwareMap.get("L claw");
+        Claw.setDirection(Servo.Direction.REVERSE);
+        Claw.setPosition(Close);
         //        opMode.time
     }
 
     public static void teleOp() throws InterruptedException {
-        if (Count>80){
             if (Driver2.x){
-                if(ArmIsUp){
-                    armServo(intakePosition,intakePosition);
-                    ArmIsUp = false;
-                    Count =0;
-                } else {
-                    armServo(scorePosition,scorePosition);
-                    ArmIsUp = true;
-                    Count =0;
-                }
+                Claw.setPosition(Close);
             }
-        } else{Count++;}
-        if (Driver2.dpad_right){
-            armServo(intakePosition, intakePosition);
-        }}
-
-    public static void armServo(double setPositionRight, double setPositionLeft) {
-        rightServo.setPosition(setPositionRight);
-        leftServo.setPosition(setPositionLeft);
-}}
+            if (Driver2.y){
+                Claw.setPosition(Open);
+            }}}
