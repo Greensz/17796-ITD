39c6ea3beb50a0eff7d770f0fed7d3041de8717e
Daniel <cortezdaniel707@gmail.com>
2024-11-22 14:31:23 -0800

    Updated Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rotator.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Rotator {
+    private static Servo Rotator;
+    private static Gamepad Driver1;
+    private static Gamepad Driver2;
+    private static double Vertical = 0;
+    private static double Horizontal = 0;
+
+    public Rotator(OpMode opMode) {
+        Driver1 = opMode.gamepad1;
+        Driver2 = opMode.gamepad2;
+        Rotator = (Servo) opMode.hardwareMap.get("Rotator");
+        Rotator.setDirection(Servo.Direction.REVERSE);
+        Rotator.setPosition(Horizontal);
+        //        opMode.time
+    }
+
+    public static void teleOp() throws InterruptedException {
+        if (Driver2.x){
+            Rotator.setPosition(Horizontal);
+        }
+        if (Driver2.y){
+            Rotator.setPosition(Vertical);
+        }}}
